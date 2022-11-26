b32a7a51f30d828f9976e94ef1463ab3aaa6299b
tracykagai-2022 <tracy.kagai@student.moringaschool.com>
2022-11-26 17:32:29 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    return Math.abs(42 - number);
+  }
+  const x = distanceFromHqInBlocks(number);
+
+  function distanceFromHqInFeet(number){
+    return Math.abs(42 - number) * 264;
+  }
+  const y = distanceFromHqInFeet(number);
+
+  function distanceTravelledInFeet(start, destination){
+    const z= Math.abs( start - destination);
+    
+    return z * 264;
+    }
+    distanceTravelledInFeet(38, 34);
+
+    function calculatesFarePrice(start, destination){
+        const z= Math.abs( start - destination);
+        let feet = z * 264;
+      if (feet <= 400) 
+      {
+        return 0;
+      }
+      else if(feet < 2000)
+      {
+        let fare = (feet - 400)*0.02
+        return fare;
+      }
+      else if (feet < 2500) 
+      {
+        return 25;
+      }
+      else
+      {
+        return 'cannot travel that far';
+      } 
+    }
+    console.log(calculatesFarePrice(50, 58));
